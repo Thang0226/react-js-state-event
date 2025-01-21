2aac541dcd95fa0e3defdbc8480742347dc065ce
Thang0226 <thang.nd0226@gmail.com>
2025-01-21 09:32:52 +0700

    add color state & page color change after 5s by componentDidMount()

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      number: 0
+      number: 0,
+      color: "orange"
     };
   }
 
@@ -18,9 +19,15 @@ class App extends Component {
     this.setState({number: this.state.number - 1});
   }
 
+  componentDidMount() {       // function without binding is auto-called after loading page
+    setTimeout(() => {
+      this.setState({color: "pink"});
+    }, 5000)
+  }
+
   render() {
     return (
-    <div style={{textAlign: "center", padding: 30}}>
+    <div style={{textAlign: "center", padding: 30, width: 400, height: 100, margin: "auto", backgroundColor: this.state.color}}>
       <button onClick={this.decrease}>Decrease</button>
       <span style={{padding: 10}}>{this.state.number}</span>
       <button onClick={this.increase}>Increase</button>
